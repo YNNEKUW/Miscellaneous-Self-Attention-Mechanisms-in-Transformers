b27d646e921ef8a61c1387dab2fb7a169bb1653f
Sologa <b04901114@ntu.edu.tw>
2020-09-03 20:55:56 +0800

    .

--- example.py
+++ example.py
@@ -79,7 +79,7 @@ class SelfAttention(nn.Module):
         context_layer = torch.matmul(attention_probs, value_layer)
         if self.keep_multihead_output: 
             self.multihead_output = context_layer
-            self.multihead_output.retain_grad()
+            self.multihead_output.retain_grad() 
 
         context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
